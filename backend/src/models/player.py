f08862503fadb49067523d548864803a79557831
loicleguen <loic.leguen@hotmail.fr>
2025-12-14 22:20:01 +0100

    Refactor Player model to remove duplicate team_id field and update Config to use from_attributes

--- backend/src/models/player.py
+++ backend/src/models/player.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 
 class Player(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    team_id: int = Field(foreign_key="team.id", ondelete="CASCADE")
     name: str
     age: Optional[int]
-    team_id: int = Field(foreign_key="team.id", ondelete="CASCADE")
 
     class Config:
         from_attributes = True
