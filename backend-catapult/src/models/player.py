54aabdebd929c9b74a6613ea10af3849c7e32a80
loicleguen <loic.leguen@hotmail.fr>
2026-01-28 20:48:42 +0100

    refactor: Remove back_populates from team relationship in Player model

--- backend-catapult/src/models/player.py
+++ backend-catapult/src/models/player.py
@@ -10,7 +10,7 @@ class Player(SQLModel, table=True):
     user_id: Optional[int] = Field(default=None, foreign_key="user.id")
     name: str
     age: Optional[int]
-    team: Optional["Team"] = Relationship(back_populates="players")
+    team: Optional["Team"] = Relationship()
 
 class PlayerCreate(SQLModel):
     name: str
