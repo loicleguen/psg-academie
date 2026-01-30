b4f6d72018b5528a2bc108e27e8d0eeb36d537ef
loicleguen <loic.leguen@hotmail.fr>
2026-01-30 14:28:22 +0100

    refactor: Remove deprecated player routes and associated imports

--- backend-catapult/app.py
+++ backend-catapult/app.py
@@ -4,7 +4,6 @@ from fastapi.responses import RedirectResponse
 from src.routes.country import router as country_router
 from src.routes.academy import router as academy_router
 from src.routes.team import router as team_router
-from src.routes.player import router as player_router
 from src.routes.catapult import router as catapult_router
 from src.routes.auth import router as auth_router
 from src.db.database import init_db
@@ -23,7 +22,6 @@ app.include_router(auth_router)
 app.include_router(country_router)
 app.include_router(academy_router)
 app.include_router(team_router)
-app.include_router(player_router)
 app.include_router(catapult_router)
 
 @app.get("/")
